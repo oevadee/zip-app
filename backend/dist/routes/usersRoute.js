be695607dcf5828087ab8b8c45504fd67a569fbf
oevadee <a.szczechura.98@gmail.com>
2021-03-24 07:00:56 +0100

    trying to fix git in the project

---
+++ backend/dist/routes/usersRoute.js
@@ -0,0 +1,1 @@
+//# sourceMappingURL=usersRoute.js.map
--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,10 +1,14 @@
-import express from 'express';
+import express, { Request, Response } from 'express';
 
 const app = express();
 const port = 8080;
 
 app.get('/', (req, res) => {
   res.send('Home')
+})
+
+app.get('/api/users', async (req: Request, res: Response): Promise<any> => {
+
 })
 
 app.listen(port, () => console.log(`Started on ${port}`));
