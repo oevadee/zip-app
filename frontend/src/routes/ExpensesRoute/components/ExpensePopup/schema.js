3b8b3aab7a0f5749a31a94012bb5c4ae4c370215
oevadee <a.szczechura.98@gmail.com>
2021-04-11 17:47:30 +0200

    added better form validation

---
+++ frontend/src/routes/ExpensesRoute/components/ExpensePopup/schema.js
@@ -0,0 +1,12 @@
+import * as yup from 'yup';
+
+const schema = yup.object().shape({
+  user: yup.string().required(),
+  value: yup.string().required(`You can't add an empty expense`),
+  details: yup
+    .string()
+    .required()
+    .max(512, 'Description is to long. You can use 512 characters.'),
+});
+
+export default schema;
