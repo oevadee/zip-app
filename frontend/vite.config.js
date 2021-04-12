2f3e6cb36b3f74bcc14612c6798939ec7064d689
oevadee <a.szczechura.98@gmail.com>
2021-04-12 18:40:10 +0200

    :

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -8,7 +8,7 @@ export default defineConfig({
     port: 3000,
     proxy: {
       '/api': {
-        target: 'http://ec2-3-140-216-201.us-east-2.compute.amazonaws.com/api',
+        target: 'https://ec2-3-140-216-201.us-east-2.compute.amazonaws.com/api',
         changeOrigin: true,
         rewrite: (path) => path.replace(/^\/api/, ''),
       },
