dc8c7d52a1c6a7ba807d48062f35b2e596f5e4dd
oevadee <a.szczechura.98@gmail.com>
2021-05-08 22:04:06 +0200

    added name input with backend support. Gotta add default name for the frontend

--- frontend/src/routes/SettingsRoute/SettingsRoute.jsx
+++ frontend/src/routes/SettingsRoute/SettingsRoute.jsx
@@ -9,32 +9,47 @@ import {
   FormLabel,
   Alert,
   AlertIcon,
+  Spinner,
 } from '@chakra-ui/react';
 import axios from 'axios';
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import { useForm } from 'react-hook-form';
 import { Header } from '../../components';
 import Card from '../../uiComponents/Card/Card';
 import CardContent from '../../uiComponents/CardContent/CardContent';
 import './SettingsRoute.scss';
-import config from "../../config";
+import config from '../../config';
+import useSWR from 'swr';
 
 const SettingsRoute = ({ user }) => {
   const [isLoading, setIsLoading] = useState(false);
   const [alert, setAlert] = useState(null);
+  const [defaults, setDefaults] = useState({ name: '' });
   const { register, handleSubmit } = useForm({
     defaultValues: {
+      name: defaults.name,
       password: '',
       confirmPassword: '',
     },
   });
+  const { data, error } = useSWR(`/api/users/profile?userId=${user.id}`);
+
+  useEffect(() => {
+    if (data) {
+      setDefaults({
+        name: data,
+      });
+    }
+  }, [data]);
+
+  console.log(defaults);
 
   const onSubmit = async (values) => {
     setIsLoading(true);
     try {
       const data = await axios.put(
         `/api/users/profile?userId=${user.id}`,
-        values,
+        values
       );
       setIsLoading(false);
       setAlert(data);
@@ -50,45 +65,60 @@ const SettingsRoute = ({ user }) => {
     }
   };
 
-  console.log(alert);
+  if (!data | error) return <Spinner color='pink' />;
 
   return (
-    <div className="settings">
-      <Header title="Settings" />
+    <div className='settings'>
+      <Header title='Settings' />
       <Card>
-        <CardContent>
-          <Heading size="md">Change your password</Heading>
-        </CardContent>
-        <Divider />
-        <CardContent>
-          <form onSubmit={handleSubmit(onSubmit)}>
+        <form onSubmit={handleSubmit(onSubmit)}>
+          <CardContent>
+            <Heading size='md'>Add your name and surname.</Heading>
+          </CardContent>
+          <Divider />
+          <CardContent>
+            <FormControl>
+              <FormLabel></FormLabel>
+              <Input
+                autoComplete='off'
+                ref={register}
+                type='text'
+                name='name'
+              />
+            </FormControl>
+          </CardContent>
+          <CardContent>
+            <Heading size='md'>Change your password</Heading>
+          </CardContent>
+          <Divider />
+          <CardContent>
             <FormControl mb={4}>
               <FormLabel>Password</FormLabel>
-              <Input ref={register} type="password" name="password" />
+              <Input ref={register} type='password' name='password' />
             </FormControl>
             <FormControl mb={4}>
               <FormLabel>Confirm Password</FormLabel>
-              <Input ref={register} type="password" name="confirmPassword" />
+              <Input ref={register} type='password' name='confirmPassword' />
               <FormHelperText>We'll never share your email.</FormHelperText>
             </FormControl>
             <Button
               isLoading={isLoading}
-              loadingText="Submitting"
-              colorScheme="teal"
-              variant="solid"
-              type="submit"
-              colorScheme="blue"
+              loadingText='Submitting'
+              colorScheme='teal'
+              variant='solid'
+              type='submit'
+              colorScheme='blue'
             >
               Submit
             </Button>
-          </form>
-        </CardContent>
+          </CardContent>
+        </form>
       </Card>
       {alert && (
         <Alert
           mt={4}
           status={alert.status === 200 ? 'success' : 'warning'}
-          variant="subtle"
+          variant='subtle'
         >
           <AlertIcon />
           {alert.data.message || alert.statusText}
