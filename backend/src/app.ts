c3b9b4cb47d72b260a885d311612deacb33970d7
oevadee <a.szczechura.98@gmail.com>
2021-07-19 01:35:44 +0200

    Add better initial console logs with chalk package

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -3,6 +3,7 @@ import cors from 'cors';
 import moduleAlias from 'module-alias';
 import config from './config';
 import path from 'path';
+import chalk from 'chalk';
 
 import { usersRoute, expensesRoute, chatRoute } from './routes';
 
@@ -29,7 +30,6 @@ app.use(
   '/static',
   express.static(path.join(__dirname, '../../uploads'), { fallthrough: false })
 );
-console.log(path.join(__dirname, '../../uploads'));
 app.use(cors(options));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
@@ -38,4 +38,11 @@ app.use('/api/users', usersRoute);
 app.use('/api/expenses', expensesRoute);
 app.use('/api/chat', chatRoute);
 
-app.listen(PORT, () => console.log(`Started on ${PORT}`));
+app.listen(PORT, () => {
+  console.clear();
+  console.log(chalk.bold.cyanBright(`Server started on port ${PORT} \n`));
+  console.log(chalk.yellow(`If you see any bugs create an Issue via:`));
+  console.log(
+    chalk.dim.cyanBright(`https://github.com/oevadee/zip-app/issues`)
+  );
+});
